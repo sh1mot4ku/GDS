bb643d922e3f7a226e47cb5994b625e354f6b29d
shiory602 <suzushioufo@gmail.com>
2021-09-09 22:24:32 -0700

    first commit

---
+++ src/components/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return(
+    <div>
+      <div>This is About page!</div>
+    </div>
+  )
+}
+
+export default AboutPage;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import TopPage from '../components/TopPage';
+import AboutPage from '../components/AboutPage';
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={TopPage} exact={true} />
+    </Switch>
+    <Switch>
+      <Route path="/about" component={AboutPage} exact={true} />
     </Switch>
   </BrowserRouter>
 )
