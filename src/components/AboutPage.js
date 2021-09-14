c25489e1abe0838d5a3d1b564fbf5c3b9d5e6a2e
shiory602 <suzushioufo@gmail.com>
2021-09-14 13:27:16 -0700

    bottom_space and center sign img

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -24,30 +24,30 @@ const AboutPage = () => {
           <h1>笑顔と共に生きる開拓者であれ</h1>
         </div>
         <div className='description'>
-          <p>
+          <p className='bottom_space'>
             人が一般的には進まない道を歩むことを <br/>
             「けもの道を行く」と表現します <br/>
             未開の地を目指した先人が創り上げた道により <br/>
             その場は大きく発展していきます <br/>
           </p>
-          <p>
+          <p className='bottom_space'>
             Tech業界でのチャレンジとは「現代のけもの道」です <br/>
             今までの常識を見直し　より便利なものを世に届ける <br/>
             そんな現場で活躍する人達と切磋琢磨しながら <br/>
             ここアメリカまでやってきました <br/>
           </p>
-          <p>
+          <p className='bottom_space'>
             自然や動物と育った僕は <br/>
             ありのままの笑顔と共に <br/>
             自分らしく生きる開拓者と出逢うチャンスを与えられ <br/>
             また自身も望んでそうなりました <br/>
           </p>
-          <p>
+          <p className='bottom_space'>
             これからもそんな人々と事業を繋げたい <br/>
             自身の強みである「出逢いを創出する」力を使って <br/>
             そう思って立ち上げた会社が　Lraough　です <br/>
           </p>
-          <p>
+          <p className='bottom_space'>
             想いに共感頂けた開拓者であるそこのあなた <br/>
             一緒に　けもの道を　笑顔で歩んで行きましょう <br/>
           </p>
@@ -56,7 +56,7 @@ const AboutPage = () => {
           </div>
         </div>
       </div>
-      <div className='main-about'>
+      {/* <div className='main-about'>
         <div className='about'>
           <h1 className='font-m'>ABOUT</h1>
           <h3>Takuya "Shimotaku" Shimomura</h3>
@@ -78,7 +78,7 @@ const AboutPage = () => {
             <a href='https://www.facebook.com/takuya.shimomura'>Facebook</a>
           </div>
         </div>
-      </div>
+      </div> */}
     </div>
   )
 }
