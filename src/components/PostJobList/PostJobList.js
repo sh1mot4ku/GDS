db9644a2c14c7800671b30dfc9f193edf341f922
Taichi I <fng.trf.kk1020@gmail.com>
2021-09-22 17:44:37 -0700

    created route and component

---
+++ src/components/PostJobList/PostJobList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostJobList = () => {
+  return (
+    <div>
+      This is page for posting job list!
+    </div>
+  )
+}
+
+export default PostJobList
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import TopPage from '../components/TopPage';
+import PostJobList from '../components/PostJobList/PostJobList';
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={TopPage} exact={true} />
+      <Route path="/post_joblist" component={PostJobList} />
     </Switch>
   </BrowserRouter>
 )
