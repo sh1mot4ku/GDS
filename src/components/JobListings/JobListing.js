31741c352eba47d575c90835092296a5d1040f45
Taichi I <fng.trf.kk1020@gmail.com>
2021-09-24 03:10:33 -0700

    create new state to show job information

--- src/components/JobListings/JobListing.js
+++ src/components/JobListings/JobListing.js
@@ -4,19 +4,33 @@ import useJobListingsContext from '../../context/jobListing-context';
 
 const JobListing = () => {
   const { jobListings, dispatchJobListings } = useJobListingsContext();
+  const [job, setJob] = useState(null);
   const idParam = useParams();
 
   useEffect(() => {
     if (jobListings && idParam) {
       const matchedJob = jobListings.filter(job => job.id === idParam);
       if (matchedJob.length === 1) {
-
+        setJob(matchedJob[0]);
       }
     }
   }, [jobListings, idParam])
 
   return (
     <div>
+      {job ? (
+        <React.Fragment>
+          <div>{job.jobTitle}</div>
+          <div>{job.companyName}</div>
+          <div>{job.employeeLocation}</div>
+          <div>{job.skills}</div>
+          <div>{job.jobDescription}</div>
+        </React.Fragment>
+      ) : (
+        <div>
+          Loading....
+        </div>
+      )}
 
     </div>
   )
