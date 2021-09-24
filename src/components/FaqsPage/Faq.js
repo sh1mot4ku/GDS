744505f21cd64b128ee385604d9292d3f8d4187f
shiory602 <suzushioufo@gmail.com>
2021-09-24 16:41:58 -0700

    props

--- src/components/FaqsPage/Faq.js
+++ src/components/FaqsPage/Faq.js
@@ -2,24 +2,28 @@
 // import AccordionSummary from '@mui/material/AccordionSummary';
 // import AccordionDetails from '@mui/material/AccordionDetails';
 const Faq = (props) => {
+  console.log(props)
   return (
     <>
-      <div className='content'>
-        <h3>{props.title}</h3>
-        <p>{props.content}</p>
-        {/* <h3>質問タイトルが入ります - 01</h3>
-        <p>人が一般的には進まない道を歩むことを「けもの道を行く」と表現します。未開の地を目指した先人が創り上げた道により、その場は大きく発展していきます。
-        Tech業界でのチャレンジとは「現代のけもの道」です。今までの常識を見直し　より便利なものを世に届ける。そんな現場で活躍する人達と切磋琢磨しながらここアメリカまでやってきました。</p> */}
-      </div>
-      {/* <Accordion disabled>
-        <AccordionSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel3a-content"
-          id="panel3a-header"
-        >
-          <Typography>Disabled Accordion</Typography>
-        </AccordionSummary>
-      </Accordion> */}
+      {props.length !== 0 && (
+        <div className='content'>
+          <h3>{props.title}</h3>
+          <p>{props.content}</p>
+          {/* <h3>質問タイトルが入ります</h3>
+          <p>人が一般的には進まない道を歩むことを「けもの道を行く」と表現します。未開の地を目指した先人が創り上げた道により、その場は大きく発展していきます。
+          Tech業界でのチャレンジとは「現代のけもの道」です。今までの常識を見直し　より便利なものを世に届ける。そんな現場で活躍する人達と切磋琢磨しながらここアメリカまでやってきました。</p> */}
+
+          {/* <Accordion disabled>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel3a-content"
+              id="panel3a-header"
+            >
+              <Typography>Disabled Accordion</Typography>
+            </AccordionSummary>
+          </Accordion> */}
+        </div>
+      )}
     </>
   )
 }
