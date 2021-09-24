1815c8bfc144828aa40f93ff136849ccf497005e
shiory602 <suzushioufo@gmail.com>
2021-09-23 23:38:02 -0700

    create new file faqs

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import TopPage from '../components/TopPage';
+import Faqs from '../components/Faqs/Faqs';
 
 const AppRouter = () => (
   <BrowserRouter>
     <Switch>
       <Route path="/" component={TopPage} exact={true} />
+      <Route path="/faqs" component={Faqs} exact={true} />
     </Switch>
   </BrowserRouter>
 )
