6d490a48a5ed2f1dbed76a37f30fdc9d183977c0
shiory602 <suzushioufo@gmail.com>
2021-10-11 23:35:34 -0700

    change code for new version

--- src/components/FaqsPage/Faq.js
+++ src/components/FaqsPage/Faq.js
@@ -1,26 +1,57 @@
 import React, { useState, useEffect } from 'react';
-import { makeStyles }  from '@material-ui/core/styles';
+// import { makeStyles }  from '@material-ui/core/styles';
+import { makeStyles } from '@mui/styles';
 
-import { Accordion, AccordionSummary, AccordionDetails, Typography } from '@material-ui/core';
-import ArrowForwardIosSharpIcon from '@material-ui/icons/ArrowForwardIosSharp';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
 
+const Accordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:not(:last-child)': {
+    borderBottom: 0,
+  },
+  '&:before': {
+    display: 'none',
+  },
+}));
 
-const useStyles = makeStyles({
-  'arrow:active': {
-    transform: 'rotate(180deg)'
-  }
-});
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(2),
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+}));
 
 
 const Faq = ({question}) => {
 
   const [expanded, setExpanded] = useState('panel1');
-  const [clicked, setClicked] = useState(false);
   
   const handleChange = (panel) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
   };
-  // const classes = useStyles();
 
   useEffect(() => {
   }, [question])
@@ -31,13 +62,9 @@ const Faq = ({question}) => {
         <div className='content'>
           <Accordion expanded={expanded === `panel${question.id}`} onChange={handleChange(`panel${question.id}`)}>
             <AccordionSummary
-              expandIcon={<ArrowForwardIosSharpIcon />}
+              // expandIcon={<ArrowForwardIosSharpIcon />}
               aria-controls={`panel${question.id}d-content`}
               id={`panel${question.id}d-header`}
-              onMouseDown={() => {
-                //クリック実施時に色変更
-                setClicked(true);
-              }}
             >
               <Typography>{question.title}</Typography>
             </AccordionSummary>
