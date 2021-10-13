9c98a46add82983d8d622e44298701fa9471fef4
shiory602 <suzushioufo@gmail.com>
2021-10-13 13:58:10 -0700

    add styled

--- src/components/FaqsPage/Faq.js
+++ src/components/FaqsPage/Faq.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 // import { makeStyles }  from '@material-ui/core/styles';
 import { makeStyles } from '@mui/styles';
+import { styled } from '@mui/material/styles';
 
 import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 import MuiAccordion from '@mui/material/Accordion';
