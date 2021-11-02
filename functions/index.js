9d59383376a7cfafc4a29678fd913a3abe525240
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-11-01 18:35:07 -0700

    add functions, thank you page

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,7 @@
 const functions = require("firebase-functions");
+const nodemailer = require("nodemailer");
+const testEmail = functions.config().testemail.email;
+const testPassword = functions.config().testemail.password;
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +10,36 @@ const functions = require("firebase-functions");
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+const mailTransport = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: testEmail,
+    pass: testPassword,
+  },
+});
+
+const adminContents = (data) => {
+  return `Global Developersのコンタクトフォームよりお問い合わせがきています。
+  Name: ${data.name}
+  Email: ${data.email}
+  Message: ${data.content}`;
+};
+
+exports.sendMail = functions
+  .region("us-central1")
+  .https.onCall((data, context) => {
+    const adminMail = {
+      from: testPassword,
+      to: testPassword,
+      subject:
+        "Global Developersのコンタクトフォームよりお問い合わせがきています。",
+      text: adminContents(data),
+    };
+    mailTransport.sendMail(adminMail, (err, info) => {
+      if (err) {
+        return console.error(`transmission error: ${err}`);
+      }
+      return console.log("Email sent successfully.");
+    });
+  });
--- src/components/Contact/ContactForm/ContactForm.js
+++ src/components/Contact/ContactForm/ContactForm.js
@@ -1,85 +1,123 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BlueSidePart from "../../BlueSidePart/BlueSidePart";
+import ThankYou from "./ThankYou";
 import { FormLabel, TextField, Button } from "@mui/material";
 import "./ContactForm.scss";
 import { useState } from "react";
+import { functions } from "../../../firebase/firebase";
 
 const ContactForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    const inquiryInfo = {
+      name,
+      email,
+      message,
+    };
+    console.log(inquiryInfo);
     console.log("submitted");
+    setName("");
+    setEmail("");
+    setMessage("");
+    setIsSubmitted(true);
   };
+
+  useEffect(() => {
+    return;
+  }, [isSubmitted]);
+
   return (
     <div className="main-contact">
       <BlueSidePart />
-      <div className="form-comtainer">
-        <h1 className="title">Contact</h1>
-        {/* <FormControl> */}
+      {!isSubmitted ? (
+        <div className="form-comtainer">
+          <h1 className="title">Contact</h1>
+          {/* <FormControl> */}
 
-        <FormLabel for="name" className="label" component="legend" required>
-          NAME
-        </FormLabel>
-        <TextField
-          id="name"
-          fullWidth
-          required
-          placeholder="Your Name"
-          variant="outlined"
-          margin="normal"
-          className="text-field"
-          size="small"
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          value={name}
-        />
-        <FormLabel for="email" className="label" component="legend" required>
-          EMAIL
-        </FormLabel>
-        <TextField
-          fullWidth
-          id="email"
-          required
-          placeholder="Email Address"
-          variant="outlined"
-          margin="normal"
-          className="text-field"
-          size="small"
-          type="email"
-          onChange={(e) => setEmail(e.target.value)}
-          value={email}
-        />
-        <FormLabel for="message" className="label" component="legend" required>
-          DESCRIPTION
-        </FormLabel>
-        <TextField
-          fullWidth
-          id="message"
-          required
-          placeholder="お問い合わせ内容を記入下さい。"
-          variant="outlined"
-          margin="normal"
-          multiline
-          rows={7}
-          className="text-field"
-          size="small"
-          type="text"
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
-        <Button
-          variant="contained"
-          className="submit-btn"
-          type="submit"
-          onSubmit={(e) => onSubmit(e)}
-        >
-          SUBMIT
-        </Button>
-        {/* </FormControl> */}
-      </div>
+          <FormLabel
+            htmlFor="name"
+            className="label"
+            component="legend"
+            required
+          >
+            NAME
+          </FormLabel>
+          <TextField
+            id="name"
+            fullWidth
+            required
+            placeholder="Your Name"
+            variant="outlined"
+            margin="normal"
+            className="text-field"
+            size="small"
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <FormLabel
+            htmlFor="email"
+            className="label"
+            component="legend"
+            required
+          >
+            EMAIL
+          </FormLabel>
+          <TextField
+            fullWidth
+            id="email"
+            required
+            placeholder="Email Address"
+            variant="outlined"
+            margin="normal"
+            className="text-field"
+            size="small"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+          <FormLabel
+            htmlFor="message"
+            className="label"
+            component="legend"
+            required
+          >
+            DESCRIPTION
+          </FormLabel>
+          <TextField
+            fullWidth
+            id="message"
+            required
+            placeholder="お問い合わせ内容を記入下さい。"
+            variant="outlined"
+            margin="normal"
+            multiline
+            rows={7}
+            className="text-field"
+            size="small"
+            type="text"
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+          <Button
+            variant="contained"
+            className="submit-btn"
+            type="submit"
+            onClick={(e) => onSubmit(e)}
+          >
+            SUBMIT
+          </Button>
+          {/* </FormControl> */}
+        </div>
+      ) : (
+        <ThankYou />
+      )}
     </div>
   );
 };
