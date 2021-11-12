15cc379fbda3b8216351a3b519d50ef1a811ce69
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-11-11 17:37:41 -0800

    add validation

--- src/components/Contact/ContactForm/ContactForm.js
+++ src/components/Contact/ContactForm/ContactForm.js
@@ -7,16 +7,19 @@ import "./ContactForm.scss";
 import { useState } from "react";
 import { functions } from "../../../firebase/firebase";
 
-const DEFAULT_INFO = {
-  name: "",
-  email: "",
-  content: "",
-};
-
 const ContactForm = () => {
-  const [inquiryInfo, setInquiryInfo] = useState(DEFAULT_INFO);
-  const [isSubmitted, setIsSubmitted] = useState(false);
-  console.log(inquiryInfo);
+  const [inquiryInfo, setInquiryInfo] = useState({
+    name: "",
+    email: "",
+    content: "",
+  });
+  const [canSubmit, setCanSubmit] = useState(false);
+  const [nameError, setNameError] = useState(null);
+  const [emailErrors, setEmailErrors] = useState(null);
+  const [contentError, setContentError] = useState(null);
+  const [showNameError, setShowNameError] = useState(false);
+  const [showEmailError, setShowEmailError] = useState(false);
+  const [showContentError, setShowContentError] = useState(false);
 
   const isEmptyOrHasWhiteSpace = (input) => {
     if (validator.isEmpty(input) || !input.match(/\S/g)) {
@@ -25,41 +28,61 @@ const ContactForm = () => {
   };
 
   const validateName = (inputName) => {
-    if (!isEmptyOrHasWhiteSpace(inputName)) return;
-    return "お名前が記入されていません";
+    if (!isEmptyOrHasWhiteSpace(inputName)) {
+      setNameError("");
+      return;
+    }
+    setNameError("お名前が記入されていません");
+    setShowNameError(true);
   };
 
   const validateEmail = (inputEmail) => {
-    let errorMessages = [];
+    let errMsgsArr = [];
     validator.trim(inputEmail);
     if (!validator.isEmail(inputEmail)) {
-      errorMessages.push("メールアドレスが無効です");
-    } else if (isEmptyOrHasWhiteSpace(inputEmail)) {
-      errorMessages.push("メールアドレスが記入されていません");
+      errMsgsArr.push("メールアドレスが無効です");
     }
-    return errorMessages;
+    if (isEmptyOrHasWhiteSpace(inputEmail)) {
+      errMsgsArr.push("メールアドレスが記入されていません");
+    }
+    setEmailErrors(errMsgsArr);
+    errMsgsArr.length !== 0 && setShowEmailError(true);
   };
 
-  const validateMessage = (inputMessage) => {};
+  const validateMessage = (inputMessage) => {
+    if (!isEmptyOrHasWhiteSpace(inputMessage)) {
+      setContentError("");
+      return;
+    }
+    setContentError("メッセージが記入されていません");
+    setShowContentError(true);
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const sendMail = functions.httpsCallable("sendMail");
-    sendMail(inquiryInfo);
-    console.log(inquiryInfo);
-    console.log("submitted");
-    setInquiryInfo(DEFAULT_INFO);
-    setIsSubmitted(true);
+    validateName(inquiryInfo.name);
+    validateEmail(inquiryInfo.email);
+    validateMessage(inquiryInfo.content);
   };
 
   useEffect(() => {
-    return;
-  }, [isSubmitted]);
+    if (nameError === null && emailErrors === null && contentError === null)
+      return;
+    if (nameError === "" && emailErrors.length === 0 && contentError === "")
+      setCanSubmit(true);
+  }, [nameError, emailErrors, contentError]);
+
+  useEffect(() => {
+    if (!canSubmit) return;
+    const sendMail = functions.httpsCallable("sendMail");
+    sendMail(inquiryInfo);
+    console.log("submitted");
+  }, [canSubmit, inquiryInfo]);
 
   return (
     <div className="main-contact">
       <BlueSidePart />
-      {!isSubmitted ? (
+      {!canSubmit ? (
         <div className="form-comtainer">
           <h1 className="title">Contact</h1>
           <FormLabel
@@ -70,6 +93,7 @@ const ContactForm = () => {
           >
             NAME
           </FormLabel>
+
           <TextField
             id="name"
             fullWidth
@@ -77,19 +101,22 @@ const ContactForm = () => {
             placeholder="Your Name"
             variant="outlined"
             margin="normal"
-            className="text-field"
+            className={!showNameError ? "text-field" : null}
             size="small"
             type="text"
-            onChange={(e) =>
+            onChange={(e) => {
+              setShowNameError(false);
               validator.isLength(e.target.value, 0, 100) &&
-              setInquiryInfo({
-                ...inquiryInfo,
-                name: e.target.value,
-              })
-            }
+                setInquiryInfo({
+                  ...inquiryInfo,
+                  name: e.target.value,
+                });
+            }}
             value={inquiryInfo.name}
           />
-          <p>お名前が記入されていません</p>
+          {nameError !== "" && showNameError && (
+            <p className="err-msg name-err">{nameError}</p>
+          )}
           <FormLabel
             htmlFor="email"
             className="label"
@@ -105,19 +132,28 @@ const ContactForm = () => {
             placeholder="Email Address"
             variant="outlined"
             margin="normal"
-            className="text-field"
+            className={!showEmailError ? "text-field" : null}
             size="small"
             type="email"
-            onChange={(e) =>
-              validator.isLength(e.target.value, 0, 100) &&
-              setInquiryInfo({
-                ...inquiryInfo,
-                email: e.target.value,
-              })
-            }
+            onChange={(e) => {
+              setShowEmailError(false);
+              validator.isLength(e.target.value, 0, 150) &&
+                setInquiryInfo({
+                  ...inquiryInfo,
+                  email: e.target.value,
+                });
+            }}
             value={inquiryInfo.email}
           />
-          <p>メールアドレスが無効です</p>
+          {emailErrors !== null && emailErrors.length !== 0 && showEmailError && (
+            <div className="email-err-wrap">
+              {emailErrors.map((errMsg, index) => (
+                <p className="err-msg email-errs" key={index}>
+                  {errMsg}
+                </p>
+              ))}
+            </div>
+          )}
           <FormLabel
             htmlFor="content"
             className="label"
@@ -135,19 +171,31 @@ const ContactForm = () => {
             margin="normal"
             multiline
             rows={7}
-            className="text-field"
+            className={!showContentError ? "text-field-content" : null}
             size="small"
             type="text"
-            onChange={(e) =>
+            onChange={(e) => {
+              setShowContentError(false);
               validator.isLength(e.target.value, 0, 1000) &&
-              setInquiryInfo({
-                ...inquiryInfo,
-                content: e.target.value,
-              })
-            }
+                setInquiryInfo({
+                  ...inquiryInfo,
+                  content: e.target.value,
+                });
+            }}
             value={inquiryInfo.content}
           />
-          <p>{inquiryInfo.content.length} / 1000</p>
+          <div
+            className={!showContentError ? "content-wrap" : "content-wrap-err"}
+          >
+            {contentError !== "" && showContentError && (
+              <p className="err-msg content-err">{contentError}</p>
+            )}
+            <p
+              className={inquiryInfo.content.length > 950 ? "color-red" : null}
+            >
+              {inquiryInfo.content.length} / 1000
+            </p>
+          </div>
           <Button
             variant="contained"
             className="submit-btn"
