b1d86865ab85cf3c785675fcb1c571996c46eecf
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-12-16 05:30:36 -0800

    put function outside

--- src/router/judgeWrapperLayout.js
+++ src/router/judgeWrapperLayout.js
@@ -6,6 +6,16 @@ import {
 } from "../data/pathnames/pathnames";
 import { isMobile } from "react-device-detect";
 
+const isPathnamePartialMatch = (pathnames, currentPath) => {
+  let isPathPartialMatch = false;
+  pathnames.forEach((pathname) => {
+    if (currentPath.includes(pathname)) {
+      isPathPartialMatch = true;
+    }
+  });
+  return isPathPartialMatch;
+};
+
 // return object that contains how layout should be
 const judgeWrapperLayout = (currentPath) => {
   const { pathnamesExactMatchHeader, pathnamesPartialMatchHeader } =
@@ -23,30 +33,23 @@ const judgeWrapperLayout = (currentPath) => {
   };
   if (isMobile && currentPath === "/joblistings_management") return layout;
 
-  const isPathnamePartialMatch = (pathnames) => {
-    let isPathPartialMatch = false;
-    pathnames.forEach((pathname) => {
-      if (currentPath.includes(pathname)) {
-        isPathPartialMatch = true;
-      }
-    });
-    return isPathPartialMatch;
-  };
-
   const isExactMatchHeader = pathnamesExactMatchHeader.includes(currentPath);
   layout["header"] =
-    isExactMatchHeader || isPathnamePartialMatch(pathnamesPartialMatchHeader);
+    isExactMatchHeader ||
+    isPathnamePartialMatch(pathnamesPartialMatchHeader, currentPath);
 
   const isExactMatchFooter = pathnamesExactMatchFooter.includes(currentPath);
   layout["footer"] =
-    isExactMatchFooter || isPathnamePartialMatch(pathnamesPartialMatchFooter);
+    isExactMatchFooter ||
+    isPathnamePartialMatch(pathnamesPartialMatchFooter, currentPath);
 
   const isExactMatchContact = pathnamesExactMatchContact.includes(currentPath);
   layout["contactSection"] = isExactMatchContact;
 
   const isExactMatchSideBar = pathnamesExactMatchSideBar.includes(currentPath);
   layout["sideBar"] =
-    isExactMatchSideBar || isPathnamePartialMatch(pathnamesPartialMatchSideBar);
+    isExactMatchSideBar ||
+    isPathnamePartialMatch(pathnamesPartialMatchSideBar, currentPath);
 
   return layout;
 };
