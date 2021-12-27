f91cd2bbeb78710c1d3a24b4d00aef272add7979
BlackStone8960 <fng.trf.kk1020@gmail.com>
2021-12-26 18:05:33 -0800

    add dropdown menu to delete/copy job listings

--- src/components/PostJobListings/JobBox.js
+++ src/components/PostJobListings/JobBox.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
+import DeleteIcon from "@mui/icons-material/Delete";
 import "./JobBox.scss";
 
 const JobBox = ({
@@ -10,39 +11,76 @@ const JobBox = ({
   jobListing,
   tags,
   id,
-}) => (
-  <div className="job-box">
-    <Link to={`/edit_joblisting/${id}`}>
-      <div className="job-wrapper">
-        <div className="job-img-wrapper">
-          <img src={photoUrl} className="job-img" alt="top-job"></img>
+}) => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    if (isOpenMenu) {
+      menuRef.current.focus();
+    }
+  }, [isOpenMenu]);
+
+  const onBlur = () => {
+    setIsOpenMenu(false);
+  };
+
+  return (
+    <div className="job-box">
+      <Link to={`/edit_joblisting/${id}`}>
+        <div className="job-wrapper">
+          <div className="job-img-wrapper">
+            <img src={photoUrl} className="job-img" alt="top-job"></img>
+          </div>
+          <div>
+            <div className="job-box-content">
+              <span className="job-title">{jobTitle}</span>
+            </div>
+            <div className="job-box-content">
+              <span className="company-name">{companyName}</span>
+            </div>
+            <div className="job-box-content">
+              <span className="location">{employeeLocation}</span>
+            </div>
+            <div className="job-box-skill-tags">
+              {Array.isArray(tags) &&
+                tags.length !== 0 &&
+                tags.map((skill) => (
+                  <div className="skill-tag-wrapper" key={skill}>
+                    <span className="skill-tag">{skill}</span>
+                  </div>
+                ))}
+            </div>
+            <div className="job-box-content">
+              <span className="short-jd">{jobListing}</span>
+            </div>
+          </div>
         </div>
-        <div>
-          <div className="job-box-content">
-            <span className="job-title">{jobTitle}</span>
+      </Link>
+      <div className="meatballs-menu-wrapper" ref={menuRef} onBlur={onBlur}>
+        {!isOpenMenu ? (
+          <div className="meatballs-menu" onClick={() => setIsOpenMenu(true)}>
+            …
           </div>
-          <div className="job-box-content">
-            <span className="company-name">{companyName}</span>
+        ) : (
+          <div className="meatballs-menu-list-wrapper">
+            <div className="meatballs-menu-list">
+              <img
+                src="/photos/chevron-right 2.svg"
+                alt="chevron"
+                className="list-icon"
+              />
+              <span>複製</span>
+            </div>
+            <div className="meatballs-menu-list" id="delete">
+              <DeleteIcon className="list-icon" />
+              <span>削除</span>
+            </div>
           </div>
-          <div className="job-box-content">
-            <span className="location">{employeeLocation}</span>
-          </div>
-          <div className="job-box-skill-tags">
-            {Array.isArray(tags) &&
-              tags.length !== 0 &&
-              tags.map((skill) => (
-                <div className="skill-tag-wrapper" key={skill}>
-                  <span className="skill-tag">{skill}</span>
-                </div>
-              ))}
-          </div>
-          <div className="job-box-content">
-            <span className="short-jd">{jobListing}</span>
-          </div>
-        </div>
+        )}
       </div>
-    </Link>
-  </div>
-);
+    </div>
+  );
+};
 
 export default JobBox;
