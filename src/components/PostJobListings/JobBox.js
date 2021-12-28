7461dd51a65ed33252593299f31ac62701685361
BlackStone8960 <fng.trf.kk1020@gmail.com>
2021-12-28 14:55:42 -0800

    edit some styles

--- src/components/PostJobListings/JobBox.js
+++ src/components/PostJobListings/JobBox.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import DeleteIcon from "@mui/icons-material/Delete";
 import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
 import IconButton from "@mui/material/IconButton";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
+import Modal from "../ui/Modal";
 import "./JobBox.scss";
 
 const JobBox = ({
@@ -16,7 +17,8 @@ const JobBox = ({
   tags,
   id,
 }) => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
@@ -25,86 +27,104 @@ const JobBox = ({
     setAnchorEl(null);
   };
 
+  const onClickDelete = () => {
+    setIsOpenModal(true);
+    handleClose();
+  };
+
   return (
-    <div className="job-box">
-      <Link to={`/edit_joblisting/${id}`}>
-        <div className="job-wrapper">
-          <div className="job-img-wrapper">
-            <img src={photoUrl} className="job-img" alt="top-job"></img>
+    <React.Fragment>
+      <div className="job-box">
+        <Link to={`/edit_joblisting/${id}`}>
+          <div className="job-wrapper">
+            <div className="job-img-wrapper">
+              <img src={photoUrl} className="job-img" alt="top-job"></img>
+            </div>
+            <div>
+              <div className="job-box-content">
+                <span className="job-title">{jobTitle}</span>
+              </div>
+              <div className="job-box-content">
+                <span className="company-name">{companyName}</span>
+              </div>
+              <div className="job-box-content">
+                <span className="location">{employeeLocation}</span>
+              </div>
+              <div className="job-box-skill-tags">
+                {Array.isArray(tags) &&
+                  tags.length !== 0 &&
+                  tags.map((skill) => (
+                    <div className="skill-tag-wrapper" key={skill}>
+                      <span className="skill-tag">{skill}</span>
+                    </div>
+                  ))}
+              </div>
+              <div className="job-box-content">
+                <span className="short-jd">{jobListing}</span>
+              </div>
+            </div>
           </div>
-          <div>
-            <div className="job-box-content">
-              <span className="job-title">{jobTitle}</span>
+        </Link>
+        <div className="meatballs-menu-wrapper">
+          <IconButton
+            aria-label="more"
+            id="long-button"
+            aria-controls={open ? "long-menu" : undefined}
+            aria-expanded={open ? "true" : undefined}
+            aria-haspopup="true"
+            onClick={handleClick}
+            className="meatballs-menu"
+          >
+            <MoreHorizIcon />
+          </IconButton>
+          <Menu
+            id="long-menu"
+            MenuListProps={{
+              "aria-labelledby": "long-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            PaperProps={{
+              style: {
+                width: "220px",
+              },
+            }}
+          >
+            <div className="meatballs-menu-list-wrapper">
+              <Link to={`copy_joblisting/${id}`}>
+                <MenuItem className="meatballs-menu-list">
+                  <img
+                    src="/photos/chevron-right 2.svg"
+                    alt="chevron"
+                    className="list-icon"
+                  />
+                  <span>複製</span>
+                </MenuItem>
+              </Link>
+              <div onClick={onClickDelete}>
+                <MenuItem className="meatballs-menu-list" id="delete">
+                  <DeleteIcon className="list-icon" />
+                  <span>削除</span>
+                </MenuItem>
+              </div>
             </div>
-            <div className="job-box-content">
-              <span className="company-name">{companyName}</span>
-            </div>
-            <div className="job-box-content">
-              <span className="location">{employeeLocation}</span>
-            </div>
-            <div className="job-box-skill-tags">
-              {Array.isArray(tags) &&
-                tags.length !== 0 &&
-                tags.map((skill) => (
-                  <div className="skill-tag-wrapper" key={skill}>
-                    <span className="skill-tag">{skill}</span>
-                  </div>
-                ))}
-            </div>
-            <div className="job-box-content">
-              <span className="short-jd">{jobListing}</span>
-            </div>
-          </div>
+          </Menu>
         </div>
-      </Link>
-      <div className="meatballs-menu-wrapper">
-        <IconButton
-          aria-label="more"
-          id="long-button"
-          aria-controls={open ? "long-menu" : undefined}
-          aria-expanded={open ? "true" : undefined}
-          aria-haspopup="true"
-          onClick={handleClick}
-          className="meatballs-menu"
-        >
-          <MoreHorizIcon />
-        </IconButton>
-        <Menu
-          id="long-menu"
-          MenuListProps={{
-            "aria-labelledby": "long-button",
-          }}
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          PaperProps={{
-            style: {
-              // maxHeight: ITEM_HEIGHT * 4.5,
-              width: "220px",
-            },
-          }}
-        >
-          <div className="meatballs-menu-list-wrapper">
-            <Link to={`copy_joblisting/${id}`}>
-              <MenuItem className="meatballs-menu-list">
-                <img
-                  src="/photos/chevron-right 2.svg"
-                  alt="chevron"
-                  className="list-icon"
-                />
-                <span>複製</span>
-              </MenuItem>
-            </Link>
-            <Link to="/">
-              <MenuItem className="meatballs-menu-list" id="delete">
-                <DeleteIcon className="list-icon" />
-                <span>削除</span>
-              </MenuItem>
-            </Link>
-          </div>
-        </Menu>
       </div>
-    </div>
+      {isOpenModal && (
+        <Modal onClose={() => setIsOpenModal(false)}>
+          <div>
+            <div>投稿を削除しますか？</div>
+            <div>
+              この操作は取り消せません。求人一覧の検索結果から投稿が削除されます。
+            </div>
+            <button>削除</button>
+            <button>キャンセル</button>
+          </div>
+        </Modal>
+      )}
+    </React.Fragment>
   );
 };
 
