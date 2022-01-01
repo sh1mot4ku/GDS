08a7e6da28a497cd9a11c4d4dc8fe830c6e25310
BlackStone8960 <fng.trf.kk1020@gmail.com>
2021-12-31 18:17:27 -0800

    add restrictions to unverified user

--- src/components/ui/UrgeApplyModal.js
+++ src/components/ui/UrgeApplyModal.js
@@ -1,31 +1,69 @@
-import React from 'react';
-import Modal from './Modal';
-import "../ui/Button.scss"
-import './ui.scss';
-import CloseIcon from '@mui/icons-material/Close';
-import { Link } from 'react-router-dom';
+import React from "react";
+import Modal from "./Modal";
+import "../ui/Button.scss";
+import "./ui.scss";
+import CloseIcon from "@mui/icons-material/Close";
+import { Link } from "react-router-dom";
+import { auth } from "../../firebase/firebase";
+import { useHistory } from "react-router-dom";
 
-const UrgeApplyModal = (props) => {
+const UrgeApplyModal = ({ onClose, isUserLoggedIn, isEmailVerified }) => {
+  const history = useHistory();
+  const onVerificateMail = async () => {
+    const user = auth.currentUser;
+    if (user) {
+      await auth.currentUser.sendEmailVerification({
+        url: `${window.location.origin}/joblistings`,
+      });
+      history.push({
+        pathname: "/send-mail-confirm",
+        state: { email: user.email },
+      });
+    }
+  };
+
   return (
     <>
-      <Modal onClose={props.onClose}>
-        <div className="urgeApply-container">
-          <CloseIcon className="close-icon" onClick={props.onClose} />
-          <img
-            src="/photos/img-joblists-detail.png"
-            className="img-joblists-detail"
-          ></img>
-          <p className="text">続きを見るには会員登録が必要です</p>
-          <Link to="/apply-developer" className="apply-link">
-            <button  className="btn-lg btn-fill">
-              無料会員登録
-            </button>
-          </Link>
-          <Link to="/login" className="login-link">
-            ログインはこちら
-          </Link>
-        </div>
-      </Modal>
+      {(!isUserLoggedIn || !isEmailVerified) && (
+        <Modal onClose={onClose}>
+          <div className="urgeApply-container">
+            <CloseIcon className="close-icon" onClick={onClose} />
+            <img
+              src="/photos/img-joblists-detail.png"
+              className="img-joblists-detail"
+            ></img>
+            {!isUserLoggedIn ? (
+              <>
+                <p className="text">続きを見るには会員登録が必要です</p>
+                <Link to="/apply-developer" className="apply-link">
+                  <button className="btn-lg btn-fill">無料会員登録</button>
+                </Link>
+                <Link to="/login" className="login-link">
+                  ログインはこちら
+                </Link>
+              </>
+            ) : (
+              !isEmailVerified && (
+                <>
+                  <p className="text">
+                    続きを見るにはメールアドレス認証が必要です。
+                    <br />
+                    メールをご確認いただき、メールに記載された URL
+                    をクリックして、
+                    <br />
+                    Global Developersへの登録を完了してください。
+                  </p>
+                  <div onClick={onVerificateMail} className="apply-link">
+                    <button className="btn-lg btn-fill">
+                      メールアドレス認証へ
+                    </button>
+                  </div>
+                </>
+              )
+            )}
+          </div>
+        </Modal>
+      )}
     </>
   );
 };
