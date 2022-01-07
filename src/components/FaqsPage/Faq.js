23b54ce6629e347b510d7a1b86729cdd6722bd49
BlackStone8960 <fng.trf.kk1020@gmail.com>
2022-01-06 22:09:02 -0800

    fix styles

--- src/components/FaqsPage/Faq.js
+++ src/components/FaqsPage/Faq.js
@@ -1,54 +1,53 @@
-import React, { useState } from 'react';
-import { styled } from '@mui/material/styles';
+import React, { useState } from "react";
+import { styled } from "@mui/material/styles";
 
-import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
-import MuiAccordion from '@mui/material/Accordion';
-import MuiAccordionSummary from '@mui/material/AccordionSummary';
-import MuiAccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
+import ArrowForwardIosSharpIcon from "@mui/icons-material/ArrowForwardIosSharp";
+import MuiAccordion from "@mui/material/Accordion";
+import MuiAccordionSummary from "@mui/material/AccordionSummary";
+import MuiAccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
 
 const Accordion = styled((props) => (
   <MuiAccordion disableGutters elevation={0} square {...props} />
 ))(({ theme }) => ({
   border: `1px solid ${theme.palette.divider}`,
-  '&:not(:last-child)': {
+  "&:not(:last-child)": {
     borderBottom: 0,
   },
-  '&:before': {
-    display: 'none',
+  "&:before": {
+    display: "none",
   },
-  '& .MuiAccordionSummary-root :hover': {
-    color: 'rgba(0, 0, 0, .7)'
+  "& .MuiAccordionSummary-root :hover": {
+    color: "rgba(0, 0, 0, .7)",
   },
 }));
 
 const AccordionSummary = styled((props) => (
   <MuiAccordionSummary
-    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: "0.9rem" }} />}
     {...props}
   />
 ))(({ theme }) => ({
   backgroundColor:
-    theme.palette.mode === 'dark'
-      ? 'rgba(255, 255, 255, .05)'
-      : 'rgba(0, 0, 0, .0)',
-  flexDirection: 'row',
-  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
-    transform: 'rotate(-90deg)',
+    theme.palette.mode === "dark"
+      ? "rgba(255, 255, 255, .05)"
+      : "rgba(0, 0, 0, .0)",
+  flexDirection: "row",
+  "& .MuiAccordionSummary-expandIconWrapper.Mui-expanded": {
+    transform: "rotate(90deg)",
   },
-  '& .MuiAccordionSummary-content': {
+  "& .MuiAccordionSummary-content": {
     marginLeft: theme.spacing(1),
   },
 }));
 
 const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
   padding: theme.spacing(2),
-  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
 }));
 
-
-const Faq = ({question}) => {
-  const [expanded, setExpanded] = useState('');
+const Faq = ({ question }) => {
+  const [expanded, setExpanded] = useState("");
 
   const handleChange = (panel) => (event, newExpanded) => {
     setExpanded(newExpanded ? panel : false);
@@ -57,21 +56,25 @@ const Faq = ({question}) => {
   return (
     <>
       {question && (
-        <div className='content'>
-          <Accordion expanded={expanded === `panel${question.id}`} onChange={handleChange(`panel${question.id}`)}>
-            <AccordionSummary aria-controls={`panel${question.id}d-content`} id={`panel${question.id}d-header`}>
+        <div className="content">
+          <Accordion
+            expanded={expanded === `panel${question.id}`}
+            onChange={handleChange(`panel${question.id}`)}
+          >
+            <AccordionSummary
+              aria-controls={`panel${question.id}d-content`}
+              id={`panel${question.id}d-header`}
+            >
               <Typography>{question.title}</Typography>
             </AccordionSummary>
             <AccordionDetails>
-              <Typography>
-                {question.content}
-              </Typography>
+              <Typography>{question.content}</Typography>
             </AccordionDetails>
           </Accordion>
         </div>
       )}
     </>
-  )
-}
+  );
+};
 
 export default Faq;
